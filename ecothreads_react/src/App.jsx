7b269cf4dec011fd55f4ad8c600f024f05f81772
mcgarretronald <mcgarretronald@gmail.com>
2024-10-13 15:21:09 +0300

    boiler plate

---
+++ ecothreads_react/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'  
+import './index.css'; // Tailwind imports should be in here
+
+function App() {
+  return (
+    <>
+      <h1 className="underline  text-3xl font-bold text-center text-blue-600">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+export default App
---
+++ ecothreads_react/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx}', // This scans all your React components
+    './index.html'          
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
